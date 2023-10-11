c3fc18271b24ae9e475f3ccd65b4ba78bf7d7709
Hannah Kim <devhkim13@gmail.com>
2023-10-10 17:46:52 -0700

    Refactor buildTree() to return the root node of BST

--- factories/tree.js
+++ factories/tree.js
@@ -1,17 +1,19 @@
 import Node from './node.js';
 
 const Tree = (arr) => {
-  let root = null;
   const sortedArr = Array.from(new Set(arr)).sort((a, b) => a - b);
 
   const prettyPrint = (node, prefix = '', isLeft = true) => {
     if (node === null) {
       return;
     }
+
     if (node.right !== null) {
       prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
     }
+
     console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+
     if (node.left !== null) {
       prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
     }
@@ -23,26 +25,19 @@ const Tree = (arr) => {
     }
 
     const midIndex = Math.floor(subArr.length / 2);
-    // Create the root note
-    const node = Node();
-    node.data = subArr[midIndex];
-
-    // Recursively construct the left subtree and make it left child of root
-    const leftSubArr = subArr.slice(0, midIndex);
-    node.left = buildTree(leftSubArr);
-    // Recursively construct the left subtree and make it left child of root
-    const rightSubArr = subArr.slice(midIndex + 1);
-    node.right = buildTree(rightSubArr);
+    const root = Node();
+    root.data = subArr[midIndex];
+    root.left = buildTree(subArr.slice(0, midIndex));
+    root.right = buildTree(subArr.slice(midIndex + 1));
 
     if (subArr.length === sortedArr.length) {
-      root = node;
       prettyPrint(root);
     }
 
-    return node;
+    return root;
   };
 
-  buildTree(sortedArr);
+  const root = buildTree(sortedArr);
 
   return {};
 };
