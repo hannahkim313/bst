118266d0089fcbe4139a23efc822a7da33e53aa8
Hannah Kim <devhkim13@gmail.com>
2023-10-11 23:25:16 -0700

    Create tests for each function of the Tree factory

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
 import Tree from './factories/tree.js';
 
-const tree = Tree([1, 4, 2, 3]);
+// initialize a new tree
+const tree = Tree([1, 10, 99, 30, 5, 23, 7]);
+
+// initial tree is balanced
+console.log(tree.isBalanced());
+
+// print out all elements in each method of traversal
+console.log(tree.levelOrder());
+console.log(tree.preOrder());
+console.log(tree.inOrder());
+console.log(tree.postOrder());
+
+// insert random numbers greater than 100
+tree.insert(101);
+tree.insert(423);
+tree.insert(380);
+tree.insert(353);
+
+// confirm the tree is now unbalanced
+console.log(tree.isBalanced());
+
+// re-balance tree
+tree.reBalance();
+
+// confirm tree is balanced
+console.log(tree.isBalanced());
+
+// print out all elements of newly balanced tree in each method of traversal
+console.log(tree.levelOrder());
+console.log(tree.preOrder());
+console.log(tree.inOrder());
+console.log(tree.postOrder());
